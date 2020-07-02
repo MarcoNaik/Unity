99d2c744243e55b1399807461a5179c3f08dff10
Marc000.99 <marcogomezf@gmail.com>
2020-07-01 22:14:31 -0400

    ranged unit and started making game controller

---
+++ jam/Assets/Scripts/Units/UnitTypes/RangeUnit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tiles;
+using Units;
+using UnityEngine;
+
+public class RangeUnit : AttackUnit
+{
+    public int range;
+    
+
+    public void Attack()
+    {
+        Attack(GetTarget());
+    }
+
+    private IUnit GetTarget()
+    {
+        List<IUnit> possiblesTargets = new List<IUnit>();
+
+        Collider[] hexOnRange = Physics.OverlapSphere(transform.position, range, 8); // 8 hex map
+
+        foreach (Collider hex in hexOnRange)
+        {
+            TileController tileController = hex.GetComponent<TileController>();
+            if (tileController.Owner == UnitController.Owner)
+            {
+                AddEnemyIn(tileController.tile.EnemyAtackers, possiblesTargets);
+            }
+            else
+            {
+                AddEnemyIn(tileController.tile.Gatherers, possiblesTargets);
+                AddEnemyIn(tileController.tile.Defenders, possiblesTargets);
+            }
+        }
+
+        return possiblesTargets[Random.Range(0, possiblesTargets.Count)];
+    }
+
+    private void AddEnemyIn(List<IUnit> list, List<IUnit> targets)
+    {
+        if (list.Count != 0)
+        {
+            foreach (IUnit enemy in list)
+            {
+                targets.Add(enemy);
+            }
+        }
+    }
+}
